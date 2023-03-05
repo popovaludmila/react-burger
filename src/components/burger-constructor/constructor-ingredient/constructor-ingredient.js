79edd028ed713ffe90313af558ce29b17e2436cb
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2023-03-05 16:16:21 +0300

    Добавила отступы в конструктор бургеров

--- src/components/burger-constructor/constructor-ingredient/constructor-ingredient.js
+++ src/components/burger-constructor/constructor-ingredient/constructor-ingredient.js
@@ -3,7 +3,7 @@ import constructorIngredientStyles from './constructor-ingredient.module.css';
 
 const ConstructorIngredient = ({item, type}) => {
     return (
-    <li className={`${constructorIngredientStyles.ingredient}  mb-4`}> 
+    <li className={`${constructorIngredientStyles.ingredient} mb-4 mr-4 mt-4`}> 
         <ConstructorElement
             text={item.name}
             type={type}
