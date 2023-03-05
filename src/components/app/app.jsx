e13910ce0801765af1cd822d976d3207e7a27f33
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2023-03-05 23:28:28 +0300

    Верстка модального окна с номером заказа

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,7 +5,7 @@ import appStyles from './app.module.css';
 
 import data from '../utils/data.js';
 import { useState } from 'react';
-import IngredientModal from '../modal/ingredient-modal/ingredient-modal';
+import OrderModal from '../modal/order-modal/order-modal';
 
 const App = () => {
   const [cart, setCart] = useState({
@@ -55,7 +55,7 @@ const App = () => {
                 <div className={appStyles.main}>
                     <BurgerIngredients items={data} cart={cart} onItemClick={onItemClick}/>
                     <BurgerConstructor cart={cart} />
-                    <IngredientModal />
+                    <OrderModal />
                 </div>
             </div>
         </main>
