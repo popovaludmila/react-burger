a00b69094003a4b203e9da267e605db0d13781f4
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2023-06-02 13:05:26 +0300

    Описание проекта

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import "./index.css";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './services/store';
 
 const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement);
 
 root.render(
   <>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </>
 )
 
